0a33ebca0614959d8a86171dd6af7bcc5910d9d6
yanakiv30 <yanakiv30@gmail.com>
2024-03-02 15:01:13 +0200

    h1-Content

---
+++ src/Ui/AppLayout.jsx
@@ -0,0 +1,14 @@
+
+import Header from './Header'
+export default function AppLayout() {
+    
+    return (
+        <div>
+           <Header /> 
+
+           <main>
+            <h1>Content</h1>
+           </main>
+        </div>
+    )
+}
